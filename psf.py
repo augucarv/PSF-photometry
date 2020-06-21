be1366937e5687c21c0be6afb55029c444d37278
augucarv <60180704+augucarv@users.noreply.github.com>
2020-06-21 13:01:41 -0300

    Computes the PSF of an astronomical image

--- psf.py
+++ psf.py
@@ -1,4 +1,4 @@
-# This code uses the catalogs made with match_py and performs PSF photometry
+# This code uses the output of match_py and performs PSF photometry
 # by using photutils's DAOPhotPSFPhotometry.
 
 # Preping the data
@@ -11,12 +11,12 @@ from astropy.io import fits
 
 # Importing the catalog from match_py
 
-catalog = Table.read('/home/augusto/Pilot/Results/MOSAIC_g_final.txt',
+catalog = Table.read('YOUR_CATALOG.txt',
                            format = 'ascii')
 
 # Importing data from the image
 
-hdu_list = fits.open('/home/augusto/Documentos/UFRGS/Pesquisa/subtracted/NGC1600_MOSAIC_g.sub.fits')
+hdu_list = fits.open('YOUR_IMAGE.fits')
 image_data = hdu_list[0].data
 
 #plt.figure()
@@ -67,47 +67,6 @@ print((toc-tic)/60)
 
 # Writing the results
 from astropy.io import ascii
-ascii.write(phot_results, 'phot_results_MOSAIC_g.txt', overwrite=True)
-ascii.write(residual, 'residual_MOSAIC_g.txt', overwrite=True)
+ascii.write(phot_results, 'phot_results.txt', overwrite=True)
+ascii.write(residual, 'residual.txt', overwrite=True)
 ##############################################################################
-
-#from photutils import DAOPhotPSFPhotometry
-#from photutils.psf import IntegratedGaussianPRF
-#from astropy.modeling.fitting import LevMarLSQFitter
-#
-#psf_model = IntegratedGaussianPRF(sigma=3.0)
-#
-#psf_model.x_0.fixed = True
-#psf_model.y_0.fixed = True
-#positions = catalog['X_IMAGE_DBL', 'Y_IMAGE_DBL','FLUX_APER']
-#positions['X_IMAGE_DBL'].name = 'x_0'
-#positions['Y_IMAGE_DBL'].name = 'y_0'
-#positions['FLUX_APER'].name = 'flux_0'
-#
-#photometry = DAOPhotPSFPhotometry(crit_separation=3, 
-#                                  threshold=3.0, 
-#                                  fwhm=1, 
-#                                  psf_model=psf_model, 
-#                                  fitshape=(11,11), 
-#                                  fitter=LevMarLSQFitter(), 
-#                                  niters=3, 
-#                                  aperture_radius=None)
-#
-#import timeit
-#tic=timeit.default_timer()
-#
-#phot_results = photometry(image=image_data,init_guesses = positions)
-#residual = photometry.get_residual_image()
-#    
-#toc=timeit.default_timer()
-#(toc-tic)/60
-
-#phot_results = Table.read('/home/augusto/phot_results.txt',
-#                           format = 'ascii')
-#
-#plt.scatter(catalog['FLUX_APER'], phot_results['flux_unc'][0:len(catalog['FLUX_APER'])])
-#plt.xlabel('Ground-truth fluxes')
-#plt.ylabel('Estimated fluxes')
-#plt.ylim((-1000,1000))
-
-
