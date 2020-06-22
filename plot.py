cd6dca73e699d3f9abd564c6da69a40075d1b07c
augucarv <60180704+augucarv@users.noreply.github.com>
2020-06-22 16:20:51 -0300

    Generate .fits file from residual table

--- plot.py
+++ plot.py
@@ -7,15 +7,16 @@ from matplotlib.colors import LogNorm
 import pandas as pd
 # Importing data from the image
 
-hdu_list = fits.open('/home/augusto/Documentos/UFRGS/Pesquisa/subtracted/NGC1600_GMOSS_i.sub.fits')
+hdu_list = fits.open('[FILE_DIRECTORY]') # Original file
 image_data = hdu_list[0].data
-residual = pd.read_table("/home/augusto/residual_GMOS_i.txt",sep="\s+")
+residual = pd.read_table("[FILE_DIRECTORY]",sep="\s+") # Table containing the residual data, obtained from psf.py
+
 # Plotting the images
 
 fig = plt.figure()
 matplotlib.rc('xtick', labelsize=20) 
 matplotlib.rc('ytick', labelsize=20) 
-fig.suptitle("NGC1600 PSF (GMOSS, i-band)", fontsize=25)
+fig.suptitle("[TITLE]", fontsize=25)
 plt.subplot(1, 2, 1)
 plt.imshow(image_data, cmap='gray_r', aspect=1,interpolation='nearest', origin='lower',norm=LogNorm())
 plt.title('Image data',fontsize=22)
@@ -28,3 +29,9 @@ plt.title('Residual Image',fontsize=22)
 plt.xlabel('px', fontsize=20)
 plt.ylabel('px', fontsize=20)
 plt.colorbar(orientation='horizontal')
+
+# Writing the FITS file
+
+hdu = fits.PrimaryHDU(residual)
+hdulist = fits.HDUList([hdu])
+hdulist.writeto('[FILE_NAME]')
