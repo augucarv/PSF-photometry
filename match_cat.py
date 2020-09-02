469c01fd3e000b42c528f8b4c08e866471b733ff
augucarv <60180704+augucarv@users.noreply.github.com>
2020-09-02 14:31:33 -0300

    Update match_cat.py

--- match_cat.py
+++ match_cat.py
@@ -5,20 +5,23 @@
 # OUTPUTS: a catalog in .txt format and a region file in .reg format to be imported in DS9
 
 #-----------------------------------------------------------------------------------------------------------------------------
-# Importing numpy and plot functions
-from matplotlib import pyplot as plt 
 
-# Importing Astropy packages
-from astropy import units as u
-from astropy import table
-from astropy.coordinates import SkyCoord, match_coordinates_sky
+# Importing packages
 from astropy.table import Table, vstack
 from astropy.io import ascii
+import numpy as np
+from scipy.spatial.distance import cdist
 
-catA= Table.read(['YOUR_CATALAOG_DIRECTORY'],
-                           format = 'ascii') # catalog A
-catB = Table.read(['YOUR_CATALAOG_DIRECTORY'],
-                           format = 'ascii') # catalog B
+# Importing the catalogs: cat_base is the base catalog whose objects we want to match
+# [!] len(cat_base) > len(cat)
+
+directory = "[WORK_DIR]" # Working directory
+name = "[FILE_NAME].fits" # FITS file
+seo1 = "def.cat" # SExtractor Output 1
+seo2 = "mex.cat" # SExtractor Output 2
+
+catA = Table.read(directory+seo1, format = 'ascii')
+catB = Table.read(directory+seo2,format = 'ascii') 
 
 if len(catA) > len(catB):
     cat_base = catA
@@ -27,53 +30,23 @@ else:
     cat_base = catB
     cat = catA
 
-# Using SkyCoord to extract the ra and dec columns
-cat1 = SkyCoord(ra=cat_base['ALPHA_J2000'], dec=cat_base['DELTA_J2000'],unit='deg')
-cat2 = SkyCoord(ra=cat['ALPHA_J2000'], dec=cat['DELTA_J2000'],unit='deg')
-
 # Matching the catalogs
 
-idx, sep, d3d = match_coordinates_sky(cat2, cat1, nthneighbor=1)
+A = np.array([cat['X_IMAGE_DBL'],cat['Y_IMAGE_DBL']]).T
+B = np.array([cat_base['X_IMAGE_DBL'],cat_base['Y_IMAGE_DBL']]).T
+dist = cdist(A, B)
+idx = np.argmin(dist > 3, axis=1)
+idx2 = idx[idx>0]
+matches = B[idx2, :] # checking the matches
+cat_base.remove_rows(idx2) # Removing the matches from B
 
-# In the above line of code, idx are the indexes of the closest matches, sep is 
-# the on-sky distance between the matches and d3d is the real-space distances
-# between the matches
+# Appending catA to new catB to create the final catalog
 
-# Finding the histogram of matches
-
-matches = cat1[idx]
-print(str('The total number of matched objects between catalogs is'))
-print(len(matches))
-
-plt.hist(sep.arcsec, histtype='step', range=(0,8))
-plt.xlabel('separation [arcsec]')
-plt.tight_layout()
-
-# Getting the indexes of the matched objects to pass these into the original catalog
-
-max_sep = 1*u.arcsec # Maximum separation constraint
-idx1, sep1, d3d1 = match_coordinates_sky(matches[sep > max_sep], cat2, nthneighbor=1)
-
-# Passing the indexes into the original catalog
-
-cat_updated = table.unique(cat[idx1],keys='NUMBER') # This catalog contains only the non-matches between cat and cat_base
-print(str('The number of objects in cat that are not in cat_base is'))
-print(len(cat_updated))
-
-print(str('The first 5 items of the updated catalog are'))
-print(cat_updated[:5])
-
-# Merging cat_updated and cat_base
-
-final_catalog = vstack([cat_base, cat_updated])
-
-print(str('The number of objects in the final catalog is'))
-print(len(final_catalog))
-
-# Writing the final catalog
-
-ascii.write(final_catalog, 'YOUR_CATALOG_NAME.txt', overwrite=True)
+final_catalog = vstack([cat_base,cat])
+final_catalog['NUMBER']=np.arange(1,len(final_catalog)+1) # rearranging the id of the items
 
 # Writing the final catalog as a .reg file to import to DS9
 
-ascii.write(final_catalog['X_IMAGE_DBL','Y_IMAGE_DBL'], 'YOUR_CATALOG_NAME.reg', overwrite=True)
+ascii.write(final_catalog, directory+name[:-5]+'.coo', overwrite=True)
+ascii.write(final_catalog['X_IMAGE_DBL','Y_IMAGE_DBL'],directory+'final.reg', overwrite=True)
+
