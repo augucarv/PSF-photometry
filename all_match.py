17fe1ec69e070f3165e9589db9f7f51833f5be80
augucarv <60180704+augucarv@users.noreply.github.com>
2020-09-26 16:17:57 -0300

    Corrected memory allocation for large catalogs

--- all_match.py
+++ all_match.py
@@ -1,5 +1,3 @@
-# This code merges 'match_cat.py' and 'psf_match.py'
-
 # This code uses Astropy package to match catalogs from SExtractor outputs.
 
 # Importing Astropy packages
@@ -11,10 +9,10 @@ from scipy.spatial.distance import cdist
 # Importing the catalogs: cat_base is the base catalog whose objects we want to match
 # [!] len(cat_base) > len(cat)
 
-directory = "[WORK_DIR]" # Working directory
-img_name = "[FILE_NAME].fits" # FITS file
-seo1 = "def.cat" # SExtractor Output 1
-seo2 = "mex.cat" # SExtractor Output 2
+directory = "WORK_DIR" # Working directory
+img_name = "IMG_NAME.fits" # FITS file
+seo1 = "FILTER1_OUT.cat" # SExtractor Output 1
+seo2 = "FILTER2_OUT.cat" # SExtractor Output 2
 
 catA = Table.read(directory+seo1, format = 'ascii')
 catB = Table.read(directory+seo2,format = 'ascii') 
@@ -30,11 +28,28 @@ else:
 
 A = np.array([cat['X_IMAGE_DBL'],cat['Y_IMAGE_DBL']]).T
 B = np.array([cat_base['X_IMAGE_DBL'],cat_base['Y_IMAGE_DBL']]).T
-dist = cdist(A, B)
-idx = np.argmin(dist > 3, axis=1)
-idx2 = idx[idx>0]
-matches = B[idx2, :] # checking the matches
-cat_base.remove_rows(idx2) # Removing the matches from B
+
+lim = 1000
+n_batches = round(len(A)/lim) # Number of batches
+
+sep = 3 # Maximum distance separation criterion [pixels]
+
+dist = []
+idx = []
+idx_matches = []
+
+for i in range(0,n_batches+1):
+    bottom_lim = i*lim
+    upper_lim = min(lim+i*lim-1,len(A))
+    dist = cdist(A[bottom_lim:upper_lim], B, metric='euclidean')
+    idx = np.argmin(dist > sep, axis=1)
+    idx2 = idx[idx>0]
+    idx_matches = np.append(idx_matches,idx2)
+    dist = []
+    idx = []
+
+cat_base.remove_rows(idx_matches.astype(int)) # Removing the matches from B
+
 
 # Appending catA to new catB to create the final catalog
 
@@ -46,36 +61,3 @@ final_catalog['NUMBER']=np.arange(1,len(final_catalog)+1) # rearranging the id o
 ascii.write(final_catalog, directory+img_name[:-5]+'.txt', overwrite=True)
 ascii.write(final_catalog['NUMBER','X_IMAGE_DBL','Y_IMAGE_DBL'], directory+img_name[:-5]+'.coo', overwrite=True)
 ascii.write(final_catalog['X_IMAGE_DBL','Y_IMAGE_DBL'],directory+'final.reg', overwrite=True) # check .reg file
-
-########## .lst file #########################################################
-
-from astropy.wcs import WCS
-from astropy.coordinates import SkyCoord, match_coordinates_sky
-
-catalog_name = directory+img_name[:-5]+'.txt'
-psf_name = 'PSF_pos.reg'
-
-# Fetching data
-
-catC = Table.read(catalog_name, format = 'ascii') # SExtractor output
-catD = Table.read(directory+psf_name, format = 'ascii') # PSF positions
-
-# Converting the PSF positions from pixels to rad,dec
-
-w = WCS(directory+img_name) # Getting the WCS info from main file
-ra,dec = w.all_pix2world(catD[0][:],catD[1][:],1)
-
-# Using SkyCoord to extract the ra and dec columns
-coordA = SkyCoord(ra=catC['ALPHA_J2000'], dec=catC['DELTA_J2000'],unit='deg')
-coordB = SkyCoord(ra, dec,unit='deg') 
-
-# Matching the catalogs
-
-#idx, sep, d3d = coord1.match_to_catalog_sky(coord2)
-idx, sep, d3d = match_coordinates_sky(coordB, coordA, nthneighbor=1)
-
-psf_match_cat = catC[idx]
-
-# Writing the final catalog as a .lst for DAOPHOT
-
-ascii.write(psf_match_cat['NUMBER','X_IMAGE_DBL','Y_IMAGE_DBL'], directory+img_name[:-5]+'.lst', overwrite=True)
